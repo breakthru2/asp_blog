76eb5680b941a575e2ea030482f93f461443d1ec
Paul Breakthrough <paulbreakthru@outlook.com>
2019-05-14 15:23:48 +0100

    Initial asp_blog
    initial asp_blog scaffolded with visual studio

---
+++ asp_blog/asp_blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp_blog.Startup))]
+namespace asp_blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
